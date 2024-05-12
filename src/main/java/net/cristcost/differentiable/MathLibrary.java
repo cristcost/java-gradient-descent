8faa350dff1acbdba16e3101308bde3e02040165
Cristcost (Vitalogy) <cristiano.costantini@gmail.com>
2024-05-12 16:15:38 -0700

    1.1: Added API for our math library

---
+++ src/main/java/net/cristcost/differentiable/MathLibrary.java
@@ -0,0 +1,31 @@
+package net.cristcost.differentiable;
+
+
+/**
+ * A Math Library performing differentiable operation.
+ */
+public class MathLibrary {
+
+  // Basic operations
+  public static Scalar sum(Scalar... operands) {
+    throw new UnsupportedOperationException("Method is not yet implemented");
+  }
+
+  public static Scalar multiply(Scalar... operands) {
+    throw new UnsupportedOperationException("Method is not yet implemented");
+  }
+
+  public static Scalar pow(Scalar base, Scalar exponent) {
+    throw new UnsupportedOperationException("Method is not yet implemented");
+  }
+
+  public static Scalar relu(Scalar operand) {
+    throw new UnsupportedOperationException("Method is not yet implemented");
+  }
+
+  // Types construction
+  public static Scalar constant(double value) {
+    throw new UnsupportedOperationException("Method is not yet implemented");
+  }
+
+}
---
+++ src/main/java/net/cristcost/differentiable/Scalar.java
@@ -0,0 +1,10 @@
+package net.cristcost.differentiable;
+
+/**
+ * Scalar interface.
+ * 
+ * Represent a primitive value type for the custom math library. 
+ */
+public interface Scalar {
+  double getValue();
+}
